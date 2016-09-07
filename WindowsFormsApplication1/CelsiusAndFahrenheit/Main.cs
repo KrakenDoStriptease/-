3984005864b66b2484fa952013baaff800382cff
Nikita <txt.nickname@gmail.com>
2016-09-08 01:33:34 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/CelsiusAndFahrenheit/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiusAndFahrenheit
+{
+    public class Main
+    {
+        public static double FahrenheitToCelsius(double y)
+        {
+            double x;
+            x = (y - 32) / 1.8;
+            return x;
+        }
+
+        public static double CelsiusToFahrenheit(double x)
+        {
+            double y;
+            y = x * 1.8 + 32;
+            return y;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CelsiusAndFahrenheit;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //Из Цельсия в фаренгейт
+        {
+            string t;
+            if (textBox1.Text.Length == 0)
+                textBox2.Text = "";
+            else if (textBox1.Focused)
+            {
+                t = textBox1.Text;
+                textBox2.Text = CelsiusAndFahrenheit.Main.CelsiusToFahrenheit(Convert.ToDouble(t)).ToString();
+            };
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) //Из Фаренгейта в Цельсий
+        {
+            string t;
+            if (textBox2.Text.Length ==0)
+                textBox1.Text = "";
+            else if (textBox2.Focused)
+            {
+                t = textBox2.Text;
+                textBox1.Text = CelsiusAndFahrenheit.Main.FahrenheitToCelsius(Convert.ToDouble(t)).ToString();
+            };
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) //Ограничение на вводимые символы у 1го бокса
+        {
+            char number = e.KeyChar;
+            if (e.KeyChar == ',' && (string.IsNullOrEmpty((sender as TextBox).Text) || (sender as TextBox).Text.Contains(',')) ||
+                (!Char.IsDigit(number) && number != 8 && number != 44))
+                e.Handled = true; //Запрет на ввод букв, нескольких запятых и запятой в начале
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e) //Ограничение на вводимые символы у 2го бокса
+        {
+            char number = e.KeyChar;
+            if (e.KeyChar == ',' && (string.IsNullOrEmpty((sender as TextBox).Text) || (sender as TextBox).Text.Contains(',')) || 
+                (!Char.IsDigit(number) && number != 8 && number != 44))
+                e.Handled = true;
+        }
+    }
+}
